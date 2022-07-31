61f7742e5c5fc5aa2f9d4b246cc0c543124539f1
khavandang <110210584+khavandang@users.noreply.github.com>
2022-08-01 06:51:10 +0700

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -29,5 +29,5 @@ sudo service ssh start
 echo "===================================="
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo create root password
-passwd
+passwd Toabdzsv3@
 echo "===================================="
